60ee1fcfa21a52ac514134c3291e3a38282606c5
brevelles@gmail.com <bernar@MacBook-Pro.local>
2020-07-04 16:06:32 +0100

    add endpoints get and post with params

---
+++ src/tasks/task.model.ts
@@ -0,0 +1,14 @@
+import { ENGINE_METHOD_ALL } from 'constants';
+
+export interface Task {
+  id: string;
+  title: string;
+  description: string;
+  status: TaskStatus;
+}
+
+export enum TaskStatus {
+  OPEN = 'OPEN',
+  IN_PROGRESS = 'IN_PROGRESS',
+  DONE = 'DONE',
+}
---
+++ src/tasks/tasks.controller.ts
@@ -0,0 +1,24 @@
+import { Controller, Get, Body, Post } from '@nestjs/common';
+import { TasksService } from './tasks.service';
+import { Task } from './task.model';
+
+@Controller('tasks')
+export class TasksController {
+  constructor(private tasksService: TasksService) {}
+
+  @Get()
+  getAllTasks(): Task[] {
+    return this.tasksService.getAllTasks();
+  }
+
+  @Post()
+  createTask(
+    @Body('title') title: string,
+    @Body('description') description: string,
+  ): Task {
+    console.log('title', title);
+    console.log('description', description);
+
+    return this.tasksService.createTask(title, description);
+  }
+}
