46ed45b875e89e8f49818ec0335a218a54353771
brevelles@gmail.com <bernar@MacBook-Pro.local>
2020-08-02 11:26:55 +0100

    signup

---
+++ src/auth/dto/auth-credentials-dto.ts
@@ -0,0 +1,18 @@
+import { IsString, MinLength, MaxLength, Matches } from 'class-validator';
+
+export class AuthCredentialsDto {
+  @IsString()
+  @MinLength(4)
+  @MaxLength(20)
+  username: string;
+
+  @IsString()
+  @MinLength(4)
+  @MaxLength(20)
+  // Passwords will contain at least 1 upper case letter, at least 1 lower case letter and at least 1 number or special character
+  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message:
+      'Password must contain at least 1 upper case letter, 1 lower case letter and 1 number or special character',
+  })
+  password: string;
+}
